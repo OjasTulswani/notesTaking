efbcabf41a730377e8fd55586dd22b8639965d79
Ojas <ojastulswani@gmail.com>
2024-09-22 00:56:36 +0530

    setting up express mongo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,25 @@
-import express, {Response, Request}  from "express";
+import express, { Express, Request, Response } from "express";
+import NoteModel from "./models/note";
 
-const app = express();
 
-app.get('/', (req : Request, res : Response) => res.send('Hello'))
+
+const app: Express = express();
+
+
+app.get("/", async (req : Request, res : Response, next) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+    throw Error("Bhai ho jaa!");
+  } catch (error) {
+      next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+
 
 export default app;
